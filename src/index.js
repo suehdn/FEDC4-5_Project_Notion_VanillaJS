4cab5ceff5058e46dc03b7d135934cde52f3302c
hyeonjun-L <beeminor60@gmail.com>
2023-06-27 17:00:33 +0900

    Init: webpack 설정

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./style.css";
+
+document.getElementById("root").innerHTML = "안녕하세요";
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+  },
+};
