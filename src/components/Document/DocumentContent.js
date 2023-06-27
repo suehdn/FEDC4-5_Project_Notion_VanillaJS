53b8e8c38b033d7ce0343bc1ef1cd5a398999f93
bbearcookie <aodem@naver.com>
2023-06-28 01:36:15 +0900

    feat: 한글 문자 아직 미완성일 경우 커서 유지를 위해 렌더 방지

--- src/components/Document/DocumentContent.js
+++ src/components/Document/DocumentContent.js
@@ -8,6 +8,7 @@ export default class DocumentContent {
 
     this.initEvents();
     this.render();
+    // this.$target.innerHTML = `<div>우하하</div><h1><div><span>이렇게 긴곳인데 찾을수있겠어요?</span></div></h2>`;
   }
 
   setState(nextState) {
@@ -16,9 +17,19 @@ export default class DocumentContent {
   }
 
   initEvents() {
-    const update = () => {};
+    let isComposing = false;
+    
+    this.$target.addEventListener('compositionstart', (e) => {
+      isComposing = true;
+    });
+
+    this.$target.addEventListener('compositionend', (e) => {
+      isComposing = false;
+      this.onChange(e.target.innerHTML);
+    });
 
     this.$target.addEventListener('input', (e) => {
+      if (isComposing) return;
       this.onChange(e.target.innerHTML);
     });
   }
@@ -29,10 +40,9 @@ export default class DocumentContent {
 
     const content = state.split('\n').join('<br>');
     const cursor = saveCursorPointer(this.$target);
-
-    // this.$target.innerHTML = `<div>우하하</div><h1><div><span>이렇게 긴곳인데 찾을수있겠어요?</span></div></h2>`;
     this.$target.innerHTML = content;
-
     restoreCursorPointer(this.$target, cursor);
+
+    console.log(content);
   }
 }
