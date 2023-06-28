c218d58fb565987fd76586866af4e4ba7ff5df8f
dlwl98 <ff981113@hanyang.ac.kr>
2023-06-28 16:01:27 +0900

    feat: 초기 진입때 documents 트리 구조를 렌더링하게 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import DocumentTreeRoot from './components/DocumentTreeRoot';
 import { route } from './domain/route';
 
 export default function App({ targetElement }) {
   this.init = () => {
     this.targetElement = targetElement;
     this.setEvent();
+    this.render();
     route();
   };
 
   this.setEvent = () => {
     window.addEventListener('popstate', route);
+  };
+
+  this.render = () => {
+    targetElement.innerHTML = `
+      <div class="document-tree-root"></div>
+    `;
+    const [documentTreeRootElement] = targetElement.children;
+    new DocumentTreeRoot({ targetElement: documentTreeRootElement });
   };
 
   this.init();
