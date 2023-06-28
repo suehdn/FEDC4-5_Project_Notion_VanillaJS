8b8ae7e1f79d81be5d5a85a7d10e08fceaf1c3ad
dlwl98 <ff981113@hanyang.ac.kr>
2023-06-28 15:09:15 +0900

    chore: api함수 한번에 export 해줌

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import { getDocuments } from './getDocuments';
+import { getDocument } from './getDocument';
+import { postDocument } from './postDocument';
+import { putDocument } from './putDocument';
+import { deleteDocument } from './deleteDocument';
+
+export { getDocuments, getDocument, postDocument, putDocument, deleteDocument };
