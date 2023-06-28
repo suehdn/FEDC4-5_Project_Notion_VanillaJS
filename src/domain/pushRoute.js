e760f42fb138f62d53baf4e36d12bcca002138ff
dlwl98 <ff981113@hanyang.ac.kr>
2023-06-28 18:40:13 +0900

    feat: history api 관련 함수들 RouteService 로직 적용 - pushRoute - replaceRoute

--- src/domain/pushRoute.js
+++ src/domain/pushRoute.js
@@ -1,6 +1,7 @@
-import { route } from './route';
+import { RouteService } from './RouteService';
 
 export function pushRoute(url) {
+  const routeService = new RouteService();
   history.pushState(null, null, url);
-  route();
+  routeService.route();
 }
--- src/domain/replaceRoute.js
+++ src/domain/replaceRoute.js
@@ -1,6 +1,7 @@
-import { route } from './route';
+import { RouteService } from './RouteService';
 
 export function replaceRoute(url) {
+  const routeService = new RouteService();
   history.replaceState(null, null, url);
-  route();
+  routeService.route();
 }
