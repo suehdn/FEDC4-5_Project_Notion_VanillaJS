adc2f23cb7fba32accba2c47d3363ad7bd74036c
oaoong <pizzachicken@kakao.com>
2023-06-28 16:47:12 +0900

    :tada: 컴포넌트 초기화

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-import { userName } from "./config/apiConfig"
-
 export default function App({ $target, initialState = [] }) {
-  this.render = () => {
-    $target.innerHTML = `
-      <h1>Hello, Vanilla!</h1>
-      <h1>Hello, ${userName}</h1>
-    `
-  }
+  this.render = () => {}
 
   this.render()
 }
---
+++ src/components/docList/index.js
@@ -0,0 +1,3 @@
+export default function DocList() {
+  return "hi"
+}
