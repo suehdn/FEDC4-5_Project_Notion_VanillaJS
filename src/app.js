ec08c33e6fe36eafda4adfe8353962a38ed1a41d
oaoong <pizzachicken@kakao.com>
2023-06-28 16:53:28 +0900

    :tada: 기본 레이아웃 설정

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
+import SideBar from "./common/sideBar"
+import NavBar from "./common/navBar"
+import MainContent from "./common/mainContent"
+
 export default function App({ $target, initialState = [] }) {
-  this.render = () => {}
+  $target.innerHTML = `
+    <div class="nav-bar"></div>
+    <div class="side-bar"></div>
+    <div class="main-content"></div>
+  `
+
+  this.render = () => {
+    new NavBar({ $target: $target.querySelector(".nav-bar") })
+    new SideBar({ $target: $target.querySelector(".side-bar") })
+    new MainContent({ $target: $target.querySelector(".main-content") })
+  }
 
   this.render()
 }
