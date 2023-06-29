f30a2655a5e3bb5ae5ac458f2d7481f8b5efdafa
backward99 <wjeeho@naver.com>
2023-06-29 12:28:46 +0900

    feat: 입력 내용을 저장하고 불러오기 위한 로컬 스토리지

---
+++ src/storage.js
@@ -0,0 +1,27 @@
+const storage = window.localStorage;
+
+export const setItem = (key, value) => {
+  try {
+    storage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export const getItem = (key, defaultValue) => {
+  try {
+    const storedValue = storage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : defaultValue;
+  } catch (error) {
+    alert(error.message);
+    return defaultValue;
+  }
+};
+
+export const removeItem = (key) => {
+  try {
+    storage.removeItem(key);
+  } catch (error) {
+    alert(error.message);
+  }
+};
