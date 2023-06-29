d9f3cf6a434b9a46720d1abfd94ab16c5b0f5593
hyesung99 <4538asd@gmail.com>
2023-06-29 16:51:54 +0900

    add : #editor, #documentTree 최초 style적용

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Component from "./Component/Component.js"
 export default class App extends Component{
 
   render(){
-    this.$target.innerHTML = `
-      <div>안녕!</div>
+    this.$target.innerHTML = 
+    `
+      <aside id='documentTree'>글 트리</aside>
+      <section id='editor'>편집하는 곳</section>
     `
   }
 }
