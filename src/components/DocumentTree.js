7ad64f44f2c6b4bc222f39f12b298eb184c498d3
dlwl98 <ff981113@hanyang.ac.kr>
2023-06-29 16:15:29 +0900

    feat: DocumentTree 컴포넌트에 트리구조 folding을 위한 props 추가

--- src/components/DocumentTree.js
+++ src/components/DocumentTree.js
@@ -1,4 +1,4 @@
-export default function DocumentTree({ targetElement, childDocuments }) {
+export default function DocumentTree({ targetElement, childDocuments, invisibleTreeSet, foldedTreeSet }) {
   this.init = () => {
     this.targetElement = targetElement;
     this.render();
@@ -9,14 +9,19 @@ export default function DocumentTree({ targetElement, childDocuments }) {
       ${childDocuments
         .map(({ id, title }) => {
           return `
-          <div class="document-tree" data-id="${id}" style="margin-left: ${15}px">
-            <span class="document-blob">
-              <span class="document-toggle">v</span>
-              ${title === '' ? '제목없음' : title}
-              <span class="new-document-btn">+</span>
-              <span class="delete-document-btn">삭제</span>
-            </span>
-          </div>`;
+            <div class="document-tree" data-id="${id}" 
+              style="margin-left: ${15}px; display: ${invisibleTreeSet.has(id) ? 'none' : 'block'}"
+            >
+              <span class="document-blob">
+                <span class="document-toggle">
+                  ${foldedTreeSet.has(id) ? '>' : 'v'}
+                </span>
+                ${title === '' ? '제목없음' : title}
+                <span class="new-document-btn">+</span>
+                <span class="delete-document-btn">삭제</span>
+              </span>
+            </div>
+          `;
         })
         .join('')}
     `;
@@ -25,6 +30,8 @@ export default function DocumentTree({ targetElement, childDocuments }) {
       new DocumentTree({
         targetElement: targetElement.querySelector(`[data-id="${id}"]`),
         childDocuments: documents,
+        invisibleTreeSet,
+        foldedTreeSet,
       });
     });
   };
