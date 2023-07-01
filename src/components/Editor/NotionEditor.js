07c40b75fd698351230ee3dffcadbfb074c5e96b
kutta97 <yhjin04170@gmail.com>
2023-07-02 02:25:47 +0900

    feat: NotionDocument 에 onEdit 이벤트 핸들러 추가 및 디바운스 적용

--- src/components/Editor/NotionEditor.js
+++ src/components/Editor/NotionEditor.js
@@ -4,13 +4,32 @@ import NotionEditorContent from '@components/Editor/Content/NotionEditorContent'
 import './NotionEditor.css';
 
 export default class NotionEditor {
-  constructor({ $target }) {
+  constructor({ $target, onEdit }) {
     this.$editor = document.createElement('section');
     this.$editor.className = 'notion-editor';
     $target.appendChild(this.$editor);
 
     this.$title = new NotionEditorTitle({ $target: this.$editor });
     this.$content = new NotionEditorContent({ $target: this.$editor });
+
+    this.initEventListener(onEdit);
+  }
+
+  getNextStateWithTarget(target) {
+    const name = target.getAttribute('name');
+    const value = name === 'title' ? target.value : target.innerHTML;
+
+    return {
+      ...this.state,
+      [name]: value,
+    };
+  }
+
+  initEventListener(callback) {
+    this.$editor.addEventListener('input', (e) => {
+      const nextState = this.getNextStateWithTarget(e.target);
+      callback?.(nextState);
+    });
   }
 
   setState(nextState) {
--- src/components/NotionDocument/NotionDocument.js
+++ src/components/NotionDocument/NotionDocument.js
@@ -1,10 +1,35 @@
+import { setItem } from '@utils/localStorage';
+
 import NotionEditor from '@components/Editor/NotionEditor';
 
 import './NotionDocument.css';
 
 export default class NotionDocument {
   constructor({ $target }) {
-    this.$editor = new NotionEditor({ $target });
+    this.$editor = new NotionEditor({
+      $target,
+      onEdit: this.onEdit.bind(this),
+    });
+
+    this.timer = null;
+  }
+
+  onEdit(document) {
+    if (this.timer !== null) {
+      clearTimeout(this.timer);
+    }
+    this.timer = setTimeout(async () => {
+      setItem(`temp-post-${document.id}`, {
+        ...document,
+        tempSaveDate: new Date(),
+      });
+    }, 1000);
+
+    const nextState = {
+      ...this.state,
+      ...document,
+    };
+    this.setState(nextState);
   }
 
   setState(nextState) {
