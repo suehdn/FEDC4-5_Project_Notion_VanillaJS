a77667aa24eb6b96b078b61659ccc6aa46a1f9b6
kutta97 <yhjin04170@gmail.com>
2023-07-02 00:47:42 +0900

    chore: @trivago/prettier-plugin-sort-imports Prettier 플러그인 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,14 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
+
+  importOrder: [
+    '^@consts/(.*)$',
+    '^@utils/(.*)$',
+    '^@components/(.*)$',
+    '^@pages/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
