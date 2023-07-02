e875802375429d50210e82c23bb06fed104cc83c
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-07-02 17:29:06 +0900

    fix: 에디터에 아무 내용이 없는 상태에서 개행하면 div에 클래스가 없던 문제 수정

--- src/utils/richEditor.js
+++ src/utils/richEditor.js
@@ -8,24 +8,35 @@ export const makeRichText = ($editor, key) => {
   const $anchorNode = selection.anchorNode;
   const text = $anchorNode.textContent;
 
-  // 맨 첫 번째 노드이면 div로 감싸줌.
-  if ($parentNode === $editor && $anchorNode.nodeType === Node.TEXT_NODE) {
-    $parentNode.innerHTML = `<div class="editor__line">${text}</div>`;
-    setCaret($parentNode.firstChild, 1);
+  // 첫 번째 줄에서 입력한 경우 div 요소로 감싸줌
+  if ($parentNode === $editor) {
+    if ($anchorNode.nodeType === Node.TEXT_NODE) {
+      // 텍스트 입력한 경우
+      $parentNode.innerHTML = `<div class="editor__line">${text}</div>`;
+      setCaret($parentNode.firstChild, 1);
+    } else if (!$parentNode.firstChild.classList.contains('editor__line')) {
+      // 비어있는 상태에서 개행해서 클래스가 없는 div 요소가 두 개 생성된 경우
+      $parentNode.innerHTML = `<div class="editor__line"><br/></div><div class="editor__line"><br/></div>`;
+      setCaret($parentNode.lastChild, 0);
+    }
   }
 
   if ($editor.innerHTML === '<br>') $editor.innerHTML = '';
 
-  if (key === 'Enter' && $line) {
-    const $newLine = document.createElement('div');
-    $newLine.classList.add('editor__line');
-    $newLine.innerHTML = '<br>';
+  // headings 태그에서 개행한 경우 다음 줄로 넘어가기
+  if (key === 'Enter') {
+    console.log($anchorNode);
+    if ($line) {
+      const $newLine = document.createElement('div');
+      $newLine.classList.add('editor__line');
+      $newLine.innerHTML = '<br>';
 
-    $parentNode.removeChild($parentNode.lastChild);
+      $parentNode.removeChild($parentNode.lastChild);
 
-    const currentNode = [...$editor.childNodes].find((n) => n === $line);
-    $editor.insertBefore($newLine, currentNode.nextSibling);
-    setCaret($newLine.firstChild, 0);
+      const currentNode = [...$editor.childNodes].find((n) => n === $line);
+      $editor.insertBefore($newLine, currentNode.nextSibling);
+      setCaret($newLine.firstChild, 0);
+    }
   }
 
   if (key === ' ' && $line) {
