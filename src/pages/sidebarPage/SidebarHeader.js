cf899b41658fca044e2f5d0c1a169f7f95f3d29a
JiYoon <angella990825@gmail.com>
2023-07-04 17:05:27 +0900

    feat: sidebar header (홈 기능) 추가

--- src/pages/sidebarPage/SidebarHeader.js
+++ src/pages/sidebarPage/SidebarHeader.js
@@ -1,1 +1,24 @@
-export default function SidebarHeader({ $target, initialState }) {}
+import { push } from '../../domain/router';
+
+export default function SidebarHeader({ $target, initialState }) {
+  const $sidebarHeader = document.createElement('div');
+  $sidebarHeader.classList.add('sidebar-header');
+  $target.appendChild($sidebarHeader);
+
+  this.state = initialState;
+
+  this.render = () => {
+    const { user } = this.state;
+    $sidebarHeader.innerHTML = `
+      <div class="user">
+        <h2 class="user-notion">📝 ${user}의 노션</h2>
+      </div>
+    `;
+  };
+
+  this.render();
+
+  $sidebarHeader.addEventListener('click', () => {
+    push('/');
+  });
+}
