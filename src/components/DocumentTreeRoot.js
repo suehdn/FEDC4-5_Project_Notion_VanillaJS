a7859331bfa832ea5a057049354c52dc3db717e9
dlwl98 <ff981113@hanyang.ac.kr>
2023-07-04 18:38:22 +0900

    fix: 접혀있는 문서 삭제시 하위 문서들 안보이는 문제 수정

--- src/components/DocumentTreeRoot.js
+++ src/components/DocumentTreeRoot.js
@@ -86,7 +86,15 @@ export default function DocumentTreeRoot({ targetElement, documents }) {
       const router = new RouteService();
       const documentTree = e.target.closest('.document-tree');
       if (confirm('페이지를 삭제하시겠습니까?')) {
-        await deleteDocument(documentTree.dataset.id);
+        const invisibleTreeSet = new Set(this.state.invisibleTreeSet.values());
+        const tobeDeletedDocument = this.state.documents.find(
+          (document) => document.id === Number(documentTree.dataset.id),
+        );
+        tobeDeletedDocument.documents.forEach((childDocument) => {
+          invisibleTreeSet.delete(childDocument.id);
+        });
+        setItem(localStorageKeys.INVISIBLE_TREES, Array.from(invisibleTreeSet));
+        await deleteDocument(tobeDeletedDocument.id);
         router.start();
       }
     });
