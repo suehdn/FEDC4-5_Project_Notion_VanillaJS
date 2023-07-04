a970a1da3a4f332830f99dd530e75b3d3e34797d
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-07-04 17:11:43 +0900

    feat: storage 추가
    -storage 기능 추가

---
+++ src/utils/storage.js
@@ -0,0 +1,17 @@
+const storage = window.localStorage;
+
+export const setItem = (key, value) => {
+  const stringifiedValue = JSON.stringify(value);
+  storage.setItem(key, stringifiedValue);
+};
+export const getItem = (key, defualtValue) => {
+  try {
+    const storedValue = storage.getItem(key);
+    if (storedValue) return JSON.parse(storedValue);
+    return defualtValue;
+  } catch (error) {
+    console.log(error.message);
+    return defualtValue;
+  }
+};
+export const removeItem = (key) => {};
