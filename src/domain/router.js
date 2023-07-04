9910a68d9ef73cd95e27f35e571b4666e84cd2e9
JiYoon <angella990825@gmail.com>
2023-07-04 16:13:08 +0900

    feat: router 기능 추가

---
+++ src/domain/router.js
@@ -0,0 +1,21 @@
+import { ROUTE_CHANGE_EVENT_NAME } from '../constants';
+
+export const initRouter = (onRoute) => {
+  window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
+    const { nextUrl } = e.detail;
+    if (nextUrl) {
+      history.pushState(null, null, nextUrl);
+      onRoute();
+    }
+  });
+};
+
+export const push = (nextUrl) => {
+  window.dispatchEvent(
+    new CustomEvent(ROUTE_CHANGE_EVENT_NAME, {
+      detail: {
+        nextUrl,
+      },
+    }),
+  );
+};
