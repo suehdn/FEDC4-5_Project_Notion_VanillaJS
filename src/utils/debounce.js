e0ebd49149d5ef909621ed7842c76f0c7962dcc8
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-07-04 17:19:02 +0900

    feat: debounce 추가
    -debounce 추가

---
+++ src/utils/debounce.js
@@ -0,0 +1,10 @@
+const setDebounce = () => {
+  let timer = null;
+  return (callback, delayTime) => {
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(callback, delayTime);
+  };
+};
+
+export const debounce = setDebounce();
