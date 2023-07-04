841dec628a0e617a6c69756a15b16e6628318741
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-07-05 00:22:15 +0900

    feat: api 구현
    -api 완전 구현

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_END_POINT = "https://kdt-frontend.programmers.co.kr";
+const API_END_POINT = "https://kdt-frontend.programmers.co.kr/documents";
 const X_USERNAME = "noCheol";
 const DEFAULT_HEADERS = { "x-username": X_USERNAME };
 
@@ -9,20 +9,28 @@ const request = async (url, opitons = {}) => {
     throw new Error("api 통신 중 이상");
   } catch (error) {
     console.log(error.message);
-    return [];
   }
 };
 
 export const getDocumentsTree = async () => {
-  return await request("/documents");
+  return await request();
 };
 
 export const getDocumentContent = async (url) => {
   return await request(url);
 };
+// 생성시 id 받아서 parent에 넣어준다.그리고 return으로 {id, title, createdAt, updataedAt}을 받는다.
+export const createDocument = async (title, parentId = null) => {
+  const bodyValue = JSON.stringify({ title, parent: parentId });
+  const response = await request("", { method: "POST", body: bodyValue });
+  return response;
+};
+// url에는 `/${id}` 같은 형식으로 들어올것이다.
+export const updateDocument = async (url, value) => {
+  const bodyValue = JSON.stringify(value);
+  await request(url, { method: "PUT", body: bodyValue });
+};
 
-export const createDocument = async () => {};
-
-export const updateDocument = async () => {};
-
-export const deleteDocument = async () => {};
+export const deleteDocument = async (url) => {
+  await request(url, { method: "DELETE" });
+};
