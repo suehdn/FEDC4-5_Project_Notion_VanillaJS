d12561caa7eace95977fa9b27bb06e336146dd3f
JiYoon <angella990825@gmail.com>
2023-07-04 17:56:54 +0900

    feat: sidebar에서 문서 추가 삭제 기능

--- src/pages/sidebarPage/SidebarDocumentTree.js
+++ src/pages/sidebarPage/SidebarDocumentTree.js
@@ -1,1 +1,62 @@
-export default function SidebarDocumentTree({ $target, initialState, addDocument, deleteDocument }) {}
+import { push } from '../../domain/router';
+
+export default function SidebarDocumentTree({ $target, initialState, addDocument, deleteDocument }) {
+  const $sidebarDocumentTree = document.createElement('div');
+  $sidebarDocumentTree.classList.add('sidebar-document__tree');
+  $target.appendChild($sidebarDocumentTree);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state(nextState);
+    this.render();
+  };
+
+  const drawSidebarDocumentTree = (tree) => {
+    const text = `
+      <ul>
+      ${tree
+        .map(
+          ({ id, title, documents }) => `
+      <div class='documents-tree'>
+        <li data-id="${id}">
+        ▶️${title}
+        <button class="add-button"> + </button>
+        <button class="delete-button"> - </button>
+        </li>
+        ${documents.map((document) => drawSidebarDocumentTree([document])).join('')}
+      </div>
+      `,
+        )
+        .join('')}
+      </ul>
+      `;
+    return text;
+  };
+
+  this.render = () => {
+    const documentsTree = drawSidebarDocumentTree(this.state);
+    const documentAddButton = `<button class="add-button">+ 페이지 추가하기</button>`;
+    $sidebarDocumentTree.innerHTML = `<div class="tree">${documentsTree}${documentAddButton}</div>`;
+  };
+
+  this.render();
+
+  $sidebarDocumentTree.addEventListener('click', (e) => {
+    const $li = e.target.closest('li');
+    const { className } = e.target;
+    const id = $li?.dataset.id;
+    if (className) {
+      if (className === 'delete-button') {
+        deleteDocument(id);
+        return;
+      } else {
+        addDocument(id, className);
+        return;
+      }
+    }
+    if ($li) {
+      push(`/documents/${id}`);
+    }
+  });
+}
