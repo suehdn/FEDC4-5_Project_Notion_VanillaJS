a47b95b4298d48585dfe1ade2b65c96ace36fc91
JiYoon <angella990825@gmail.com>
2023-07-04 17:59:13 +0900

    feat: HomePage 구현

--- src/pages/mainPage/HomePage.js
+++ src/pages/mainPage/HomePage.js
@@ -1,1 +1,20 @@
-export default function HomePage({ $target }) {}
+import { homePageMessages } from '../../constants';
+
+export default function HomePage({ $target }) {
+  const $homePage = document.createElement('div');
+  $homePage.classList.add('home-page');
+
+  this.render = () => {
+    $target.appendChild($homePage);
+    $homePage.innerHTML = `
+    <h1 class="home-title">📝${homePageMessages.HOME_TITLE}</h1>
+    <ul class="home-list">
+      <li class="home-list__text">✅${homePageMessages.ADD_PAGE_MESSAGE}</li>
+      <li class="home-list__text">✅${homePageMessages.ADD_BUTTON_MESSAGE} ${homePageMessages.DELETE_BUTTON_MESSAGE}</li>
+      <li class="home-list__text">✅${homePageMessages.BACK_TO_HOME}</li>
+    </ul>
+    `;
+  };
+
+  this.render();
+}
