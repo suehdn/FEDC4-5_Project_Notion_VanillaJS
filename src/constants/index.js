0ef0f63e5e7c37ef29d108bf07a4f8792e8e5389
JiYoon <angella990825@gmail.com>
2023-07-04 15:24:08 +0900

    feat: 상수 관련 파일 추가

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+export const errorMessages = {
+  NEED_NEW_KEYWORD: 'new 키워드를 붙여주세요!',
+  API_FAILURE_MESSAGE: 'API 처리 중 뭔가 이상합니다!',
+  API_ERROR_MESSAGE: '무엇인가 이상합니다!',
+  NEED_ARRAY_TYPE: '배열로 입력해야 합니다!',
+  NEED_STRING_TYPE: '제목과 내용은 문자열이어야 합니다!',
+};
+
+export const homePageMessages = {
+  HOME_TITLE: '노션 클로닝 프로젝트',
+  ADD_PAGE_MESSAGE: '페이지 추가 버튼으로 새 글을 작성해 보세요.',
+  ADD_BUTTON_MESSAGE: '➕로 글을 추가하거나',
+  DELETE_BUTTON_MESSAGE: '➖로 글을 삭제할 수 있어요.',
+  BACK_TO_HOME: '유저명을 누르면 홈으로 돌아올 수 있어요!',
+};
+
+export const TEMP_DATA_MESSAGE = '저장되지 않은 임시 데이터가 있습니다 불러올까요?';
+
+export const API_END_POINT = 'https://kdt-frontend.programmers.co.kr';
+export const USERNAME = 'jiyoon';
+
+export const ROUTE_CHANGE_EVENT_NAME = 'route-change';
+
+export const editDocumentMessages = {
+  INITIAL_DOCUMENT_TITLE: '제목 없음',
+  DOCUMENT_TITLE_PLACEHOLDER: '제목을 입력하세요',
+  DOCUMENT_CONTENT_PLACEHOLDER: '내용을 입력하세요',
+};
