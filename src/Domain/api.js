cb14d544f77f33c86afc2057e0922b953d6778bf
Jeong-Taeho <117630589+Jeong-Taeho@users.noreply.github.com>
2023-07-05 23:27:15 +0900

    feat : api 호출

---
+++ src/Domain/api.js
@@ -0,0 +1,21 @@
+const API_END_POINT = 'https://kdt-frontend.programmers.co.kr'
+
+export const request = async (url, options = {}) => {
+    try {
+        const res = await fetch(`${API_END_POINT}${url}`, {
+            ...options,
+            headers: {
+                'Content-Type': 'application/json',
+                'x-username': 'jeongtaeho',
+            },
+        })
+
+        if (res.ok) {
+            return await res.json()
+        }
+
+        throw new Error('API 호출 오류')
+    } catch (e) {
+        alert(e.message)
+    }
+}
