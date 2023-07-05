80e2ed544b8522f09f26addabc0ddc0096195450
Park Nayeon <within197@naver.com>
2023-07-05 18:22:12 +0900

    [feat] App 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import { fetchAPI } from './util/api.js';
-export default function App({ $target }) {
-  const request = async () => {
-    let res = await fetchAPI('/');
-    console.log(res);
-  };
+import Category from './components/DocumentList.js';
+import PostEditPage from './components/PostEditPage.js';
+import { initRouter } from './router.js';
 
-  request();
+export default function App({ $target }) {
+  $target.style.display = 'flex';
+  const $left = document.createElement('div');
+  $left.className = 'left';
+  $target.appendChild($left);
+
+  const $right = document.createElement('div');
+  $right.className = 'right';
+  $target.appendChild($right);
+
+  const postEditPage = new PostEditPage({
+    $target: $right,
+    initialState: {
+      postId: 'new',
+      post: { title: '', content: '' },
+    },
+  });
+
+  new Category({
+    $target: $left,
+    initialState: {
+      template: '',
+      documents: [],
+    },
+  });
+
+  this.route = () => {
+    $right.innerHTML = '';
+    const { pathname } = window.location;
+
+    if (pathname.indexOf('/posts/') === 0) {
+      const [, , id] = pathname.split('/');
+      postEditPage.setState({
+        ...postEditPage.state,
+        postId: id,
+      });
+    } else if (pathname === '/') {
+      $right.innerHTML = '';
+    }
+  };
+  this.route();
+  initRouter(() => this.route());
 }
