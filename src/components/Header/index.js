727fe98708dcfa90ee0f38014b30bfbb5c2491c3
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-07-06 10:06:44 +0900

    Delete: 폴더 디렉토리 구조 변경

--- src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-
-export default function Header({ $target }) {
-  const $Header = document.createElement('header');
-  $Header.className = 'Header';
-  $Header.textContent = '노션 클로닝 웹 앱입니당.';
-
-  $target.appendChild($Header);
-}
