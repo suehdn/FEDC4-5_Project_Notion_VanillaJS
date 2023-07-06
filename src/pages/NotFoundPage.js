c6a34b239b364dad4e153cc59b7aefa30f55fe04
dlwl98 <ff981113@hanyang.ac.kr>
2023-07-06 11:15:18 +0900

    feat: NotFoundPage 홈버튼 구현 및 스타일링

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,14 +1,34 @@
 import validateComponent from '../utils/validateComponent';
+import { RouteService } from '../utils/RouteService';
+
+const homeBtnHandler = (e) => {
+  if (!e.target.closest('.not-found-page-home-btn')) return;
+  const router = new RouteService();
+  router.push('/');
+};
 
 export default function NotFoundPage({ targetElement }) {
   validateComponent(this, NotFoundPage);
   this.init = () => {
     this.targetElement = targetElement;
+    this.setEvent();
     this.render();
   };
 
+  this.setEvent = () => {
+    targetElement.removeEventListener('click', homeBtnHandler);
+    targetElement.addEventListener('click', homeBtnHandler);
+  };
+
   this.render = () => {
-    targetElement.innerHTML = '존재하지 않는 페이지 입니다.';
+    targetElement.innerHTML = `
+      <div class="not-found-page-outer">
+        <span class="not-found-page-inner">
+          <div class="not-found-page-home-btn">📘 이진욱의 Notion</div>
+          존재하지 않는 페이지입니다.
+        </span>
+      </div>
+    `;
   };
 
   this.init();
